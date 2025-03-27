ebb8d09c9cb5dee5cdab33a8e28e4a3cbe035797
gagalvez <gagalvezv@gmail.com>
2025-03-27 20:05:09 -0300

    First push, learning tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav class="" >
+            <h1>Logo</h1>
+            <ul>
+                <li>nav1</li>
+                <li>nav2</li>
+                <li>nav3</li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Navbar
