46ff289bdc8c23f34e376ccf8863fcf41237b218
gagalvez <gagalvezv@gmail.com>
2025-04-03 00:05:08 -0300

    Few changes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Article = () => {
   return (
-    <div className="bg-amber-300 w-full h-full p-28">
-      <h1 className="text-center">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum natus
-        provident dignissimos. Voluptas, vero consequuntur? Dolor, eos
-        voluptatibus sequi facere voluptates quasi et illum harum perspiciatis
-        explicabo exercitationem porro pariatur!
+    <article className="grid justify-items-center bg-white w-full h-2/4 pt-12">
+      <h1 className="text-center w-2/4 font-bold text-4xl">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
       </h1>
-    </div>
+      <div className="flex justify-center">
+        <p className="text-center w-2/4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam necessitatibus nemo labore ut, fugit, consequatur animi suscipit dolorem consectetur, ipsa quis neque ullam veniam! Recusandae aliquam commodi quod praesentium quis!</p>
+      </div>
+    </article>
   );
 };
 
